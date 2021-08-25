8dbf2368f967192f1472c0638de7c4e6620facf7
HUANGJIEHUI <847884008@qq.com>
2021-08-26 00:22:14 +0800

    解决合并问题

--- src/components/Stop.js
+++ src/components/Stop.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';  //jiejuehebingwenti
+import React, { Component } from 'react';  //解决合并问题2
 
 export default class Stop extends Component { //测试合并之后其他地方修改是否能合并进去
   componentDidMount() {
